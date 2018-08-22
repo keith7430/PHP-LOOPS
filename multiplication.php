fc7124e193ee9bc80952c39de53a361535ab1911
Keith Brown <mkpindura@gmail.com>
2018-08-22 16:10:47 +0200

    Add files via upload
    try to this without looking at the solution

---
+++ multiplication.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Challenge: using loops</title>
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>Multiplication Table</h1>
+<table>
+
+	<?php 
+
+	//creating first row of tables
+     
+     echo '<tr>';
+     echo "<th>&nbsp;</th>";
+     for ( $col = 1; $col <= 12 ; $col++) :
+        	echo "<th>$col</th>";
+        endfor;
+    
+    echo'</tr>';
+    //creating the remaining rows
+    for ( $row = 1, $col = 1; $row <= 12; $row++ ) :
+    	echo '<tr>';
+    	//First cell is a table header
+    	if ( $col == 1) {
+           echo "<th>$row</th>";
+    	}
+    	while ( $col <= 12) :
+    		echo '<td>'. $row * $col++ . '</td>';
+    	endwhile;
+    	echo '</tr>';
+    	//reset $col at the end of each row
+    	$col = 1;
+    endfor;
+?>
+</table>	
+
+
+
+
+
+</body>
+</html>
